4c270464a7863820209b6770ff70ded6e2743b76
Jung Sung Wook <jswcyber@naver.com>
2023-09-05 11:37:12 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 30.03 ms, Memory: 50.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -0,0 +1,55 @@
+let zero = 0;
+let one = 0;
+function solution(arr) {
+    quart(arr)
+    return [zero, one];
+}
+
+function quart(arr) {
+    if (arr.length == 1){
+      if(arr[0]==0) zero++;
+      else one++;
+      return arr;
+    }
+    arr_type = isBinaryMatrix(arr);
+    if(arr_type[0]){
+      if(arr_type[1]==0) zero++;
+      else one++;
+      return;
+    }
+    const half = arr.length / 2;
+    let arr1 = [],
+        arr2 = [],
+        arr3 = [],
+        arr4 = [];
+    // 4분할
+    for (let i = 0; i < half; i++) {
+        const next = Number(i + arr.length / 2);
+        arr1.push(arr[i].slice(0, half));
+        arr2.push(arr[i].slice(half, arr.length));
+        arr3.push(arr[next].slice(0, half));
+        arr4.push(arr[next].slice(half, arr.length));
+    }
+
+    arr1 = quart(arr1);
+    arr2 = quart(arr2);
+    arr3 = quart(arr3);
+    arr4 = quart(arr4);
+}
+
+function isBinaryMatrix(arr) {
+    const type = arr[0][0]
+    let o = 0;
+    let z = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] === 0) {
+                z++;
+            } else if (arr[i][j] === 1) {
+                o++;
+            }
+        }
+    }
+    if(o>0 && z>0) return [false, undefined];
+    else return [true, type];
+}
